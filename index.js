30908bf0649f08153afefb1829d9a0d6033ef68f
Zeyad Etman <zeyadetman@gmail.com>
2020-04-09 12:38:25 +0200

    Finalize the package

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+#!/usr/bin/env node
+
 const main = require("./main");
 const argv = require("yargs").argv;
 
-console.log(argv);
-main();
+const { unit, currency, size, u, c, s } = argv;
+const cli = require("meow")(
+  `
+  Usage: goldpricez [options]
+
+  You have to set at least one of the following options 
+
+  Options:
+  
+    -u, --unit         Unit 'K' of the gold [ 24, 23, 22, ..., 9 ], default = 21 
+    -c, --currency     Currency of the price, default = EGP
+    -s, --size         Size of the calculated [gram, kg, ounce, grain]
+`,
+  {
+    boolean: ["help", "version"],
+    alias: { h: "help", v: "version", u: "unit", c: "currency", s: "size" },
+  }
+);
+
+if (Object.keys(argv).length <= 2) {
+  cli.showHelp();
+}
+
+main({ unit: unit || u, currency: currency || c, size: size || s });
--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+const ora = require("ora");
 
-module.exports = async (unit, currency) => {
-  console.log(unit, currency);
+module.exports = async ({ unit = 21, size = "gram", currency = "egp" }) => {
+  const spinner = ora().start();
+  spinner.color = "yellow";
+  spinner.text = "Getting Gold Price";
 
-  const getPricesTable = async (unit) => {
+  const getPricesTable = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`http://goldpricez.com/us/${unit}`, {
+    await page.goto(`http://goldpricez.com/us/${size}`, {
       waitUntil: "networkidle2",
     });
 
@@ -32,8 +36,7 @@ module.exports = async (unit, currency) => {
       }
     });
 
-    console.log(goldPricesInUsd);
-    return goldPricesInUsd;
+    return goldPricesInUsd[unit.toString()];
   };
 
   const parsePricesHtmlCode = (htmlCode) => {
@@ -48,10 +51,24 @@ module.exports = async (unit, currency) => {
       .split("=")
       .flatMap((a) => a.split("USD"));
 
-    getGoldPricesInUsd(pureTableValues);
+    return pureTableValues;
   };
 
-  const pageBody = await getPricesTable("gram");
+  const convertFromUsd = async (userCurrency) => {
+    const converterValue = await fetch(
+      `https://free.currconv.com/api/v7/convert?q=USD_${userCurrency.toUpperCase()}&compact=ultra&apiKey=d021687a36d0c4af3c45`
+    );
+    const currencyValue = await converterValue.json();
+    return Object.values(currencyValue)[0];
+  };
 
-  parsePricesHtmlCode(pageBody);
+  const pageBody = await getPricesTable();
+  const pureTableValues = parsePricesHtmlCode(pageBody);
+  const pricesInUsd = getGoldPricesInUsd(pureTableValues);
+  const finalValue = await convertFromUsd(currency);
+  const result = `${unit}K = ${
+    finalValue * pricesInUsd
+  } ${currency.toUpperCase()}`;
+  spinner.succeed(result);
+  return result;
 };
